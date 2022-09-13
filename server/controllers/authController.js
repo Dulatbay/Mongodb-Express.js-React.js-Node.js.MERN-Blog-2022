122317b74c92c15d112bcda237bacf05e58faaee
Akhan <adulatbai@bk.ru>
2022-09-13 19:39:05 +0600

    Commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     const { userName, password } = req.body;
@@ -26,12 +27,37 @@ export const register = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    return res.status(400).json({message:"Ошибка при создании пользователя", error: error})
+    return res
+      .status(400)
+      .json({ message: "Ошибка при создании пользователя", error: error });
   }
 };
 
 export const login = async (req, res) => {
   try {
+    const { userName, password } = req.body;
+    const user = User.findOne({ userName });
+    if (user == null) {
+      return res
+        .status(400)
+        .json({ message: "Такого пользователя не существует!" });
+    }
+    const isCorrectedPassword = await bcrypt.compare(password, user.password);
+    if (isCorrectedPassword == false) {
+      return res.status(400).json({ message: "Пароли не совпадают!" });
+    }
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET,
+      {expiresIn: '30d'}
+    );
+    return res.json({
+      token,
+      user,
+      message: "Вы вошли в систему"
+    })
   } catch (error) {
     console.log(error);
   }
