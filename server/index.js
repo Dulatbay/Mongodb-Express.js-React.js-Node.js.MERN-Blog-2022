93cfc32da0ead0a59a240149622a30dd917388d2
Akhan <adulatbai@bk.ru>
2022-09-13 18:55:04 +0600

    Backend development

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ const connString = process.env.DB_CONNECTION_STR
 app.use(cors())
 app.use(express.json())
 
-
+app.get('/', (req, res)=>{
+    res.json({message: 'All is fine'})
+})
 
 
 async function start(){
